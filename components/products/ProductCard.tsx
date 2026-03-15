9e102aabf2cda9f9dcf83e889074960b3c428727
StephenTetteh-Nirvana <stephentetteh2616@gmail.com>
2026-03-15 03:07:13 -0700

    added section to display the product name,price and a button to add to cart

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,16 +1,34 @@
 import { Card } from "../ui/card"
+import { Button } from "../ui/button"
 import Image from "next/image"
+import { Plus } from "lucide-react"
 
 const ProductCard = () => {
     return (
-        <Card className="p-0 m-0 h-[200px] lg:h-[300px] group border-0 bg-transparent shadow-none">
-            <Image
-                src="/images/productImage6.jpg"
-                width={700}
-                height={700}
-                alt='productImage' 
-                className="w-full h-full object-cover object-center transition-transform duration-300 group-hover:scale-105 cursor-pointer brightness-95"
-            />
+        <Card 
+          className="relative p-0 m-0 h-[200px] lg:h-[300px] group border-0 
+          bg-transparent shadow-none rounded-sm"
+        >
+          <Image
+            src="/images/productImage6.jpg"
+            width={700}
+            height={700}
+            alt='productImage' 
+            className="w-full h-full object-cover object-center 
+            transition-transform duration-300 group-hover:scale-105 cursor-pointer"
+          />
+
+          <div className="absolute bottom-0 w-full bg-black/80 shadow-lg px-2 py-2 flex items-center justify-between">
+            <div className="flex flex-col">
+              <p className="text-white text-[14px] font-bold line-clamp-1 tracking-wider">Nike BackPack</p>
+              <p className="text-white">GHS 400</p>
+            </div>
+            <Button 
+              className="bg-white flex items-center justify-center rounded-full h-8 w-8 cursor-pointer"
+            >
+              <Plus color="black"/>
+            </Button>
+          </div>
         </Card>
     )
 }
