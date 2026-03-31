316aa4ef709b7a2b52ceed6fdd2faee8963acc8d
StephenTetteh-Nirvana <stephentetteh2616@gmail.com>
2026-03-31 12:26:44 -0700

    changed banner text

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,52 +1,35 @@
 import Image from "next/image"
 import { Button } from "../ui/button"
+import { Input } from "../ui/input"
 
 const Banner = () => {
   return (
-    <div className="relative h-[70vh] w-full mt-15">
+    <div className="relative h-[70vh] px-10 rounded-lg w-full mt-15">
 
       {/* MOBILE VIEW  */}
-      <div className="sm:hidden">
+      <div className=" ">
         <Image
           src="/images/productImage5.jpg"
           alt="AdImage"
           fill
-          className="object-cover brightness-40"
+          className="object-cover brightness-20"
         />
   
-        <div className="flex flex-col gap-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[80%]">
-          <p className="text-white text-center text-2xl font-semibold  w-full">
-            Upgrade your wardrobe with our latest arrivals and timeless essentials.
+        <div className="flex flex-col gap-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70%] md:w-[40%]">
+          <p className="text-white text-center w-full leading-7 md:leading-9 md:text-[20px]">
+            Be the first to get alerted about new arrivals, dicounts and exclusive offers. Subscribe to our newsletter and stay ahead of the fashion curve.
           </p>
+          <Input className="border-0 border-b rounded-none focus-visible:border-b focus-visible:ring-0 focus-visible:ring-0 text-slate-300 placeholder:text-slate-300 text-[14px]" 
+          placeholder="Enter your email here for updates"/>
           <Button 
             variant="outline"
-            className="bg-white rounded-full py-2 cursor-pointer w-[200px] mx-auto text-white font-bold text-black"
+            className="bg-white rounded-sm py-2 cursor-pointer w-full mx-auto text-white font-bold text-black mt-5"
           >
-            SHOP ALL
+            SUBSCRIBE NOW
           </Button>
         </div>
       </div>
-
-      {/* TABLETS AND LAPTOPS VIEW  */}
-      <div className="hidden sm:flex sm:justify-between h-full">
-        <div className="bg-primary flex flex-col items-start gap-3 justify-center px-15">
-          <p className="text-white text-3xl font-semibold w-[80%]">
-            Upgrade your wardrobe with our latest arrivals and timeless essentials.
-          </p>
-          <button className="bg-white rounded-md py-2 w-[50%] font-semibold">SHOP ALL</button>
-        </div>
-
-        <div className="w-full h-full">
-          <Image
-            src="/images/productImage5.jpg"
-            alt="AdImage"
-            width={900}
-            height={200}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      </div>
-
+  
     </div>
   )
 }
